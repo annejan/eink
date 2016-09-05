82e693740aa3dfb219db8ee121314c74e9cbbeff
Anne Jan Brouwer <annejan@noprotocol.com>
2016-09-06 01:40:41 +0200

    Commented opcodes

--- src/eink.cpp
+++ src/eink.cpp
@@ -62,15 +62,22 @@ void initSPD2701(bool fast = false)
   writeCommand(0x4F);//set RAM y address count to 0;
   writeData(0xAB);
   writeCommand(0x21);//bypass RAM data
-  writeData(0x03);
+  writeData(0x03);      // InitialUpdate_SourceControl=3=GS0,GS3; Disable Bypass - clear with inverted image, than draw the image.
+  // writeData(0x83);      // InitialUpdate_SourceControl=3=GS0,GS3; Enable Bypass (clear with white bg)
+  // writeData(0xB3);      // InitialUpdate_SourceControl=3=GS0,GS3; Enable Bypass; A[5:4]=11 (clear with black bg)
   writeCommand(0xF0);//booster feedback used
   writeData(0x1F);
   writeCommand(0x2C);//vcom voltage
   writeData(0xA0);
   writeCommand(0x3C);//board voltage ? border waveform ?
-  writeData(0x63);
+  writeData(0x63);          // fix white; OK
+  // writeData(0x43);          // leaves gray border
+  // writeData(0x03);          // Black-White transition; OK
   writeCommand(0x22);//display updata sequence option ,in page 33
-  writeData(0xC4);//enable sequence: clk -> CP -> LUT -> pattern display
+  // writeData(0xC4);      //enable sequence: clk -> CP -> LUT -> pattern display
+  // writeData(0xC0);      // enable clock, enable CP
+  // writeData(0xCC);      // enable clock, enable CP, display initial (black/white transient several times), display pattern
+  writeData(0xC7);      // enable clock, enable CP, display pattern; Disable CP, disable clock. OK
   writeLUT(fast);
 }
 
@@ -124,6 +131,6 @@ void loop() {
   displayImage(5);
   delay(5000);
   resetDisplay();
-  initSPD2701(true);
+  initSPD2701(true); // go faster
   displayImage(1);
 }
