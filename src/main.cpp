8902de1e63cd9f812cf7fba53ad26b2a06ef30a5
Anne Jan Brouwer <annejan@noprotocol.com>
2016-09-14 21:46:39 +0200

    Updated parttest, this is a weird demo and slower than the 'full update' thing

--- src/main.cpp
+++ src/main.cpp
@@ -17,42 +17,47 @@ void setup() {
   displayImage(pictures[0]);
 }
 
-#ifdef GDEH029A1_H
+#ifdef GDE_DRIVER_H
+int speed = 500;
+
 void parttest()
 {
-	unsigned int i,j,k;
+	unsigned int h,i,j,k;
 	writeLUT(true);
 	powerOn();
 	setRamArea(0x00,0x0f,0x27,0x01,0x00,0x00);
 	setRamPointer(0x00,0x27,0x01);	// set ram
   writeDispRamMono(128, 296, 0xff);	// white
-  delay(10000);
+  delay(speed);
   writeDispRamMono(128, 296, 0xff);	// white
   updateDisplayPartial();
-  delay(10000);
+  delay(speed);
 	while(1)
 	{
 	  partialDisplay(0x00,0x0f,0x27,0x01,0x00,0x0);	// set ram
-		writeDispRam(128,296, gImage_logo);
+		writeDispRam(128,296, pictures[0]);
 		updateDisplayPartial();
-		delay(10000);
+		delay(speed);
 		partialDisplay(0x00,0x0f,0x27,0x01,0x00,0x00);	// set ram
-    writeDispRam(128,296, gImage_logo);
-		delay(10000);
+    writeDispRam(128,296, pictures[0]);
+		delay(speed);
 
-		for(i=0;i<8;i++)
-		{
-		  k=0x127-i*32-20;
-			j=k-32;
-			partialDisplay(0x0e,0x0e,k%256,k/256,j%256,j/256);	// set ram
-			writeDispRamMono(8, 32, 0x00);	// white
-	 		updateDisplayPartial();
-			delay(2000);
-			partialDisplay(0x0e,0x0e,k%256,k/256,j%256,j/256);	// set ram
-			writeDispRamMono(8, 32, 0x00);	// white
-			delay(1000);
-		}
-	}
+    for(h=0;h<8;h++)
+    {
+      for(i=0;i<8;i++)
+    	{
+  		  k=0x127-i*32-20;
+  			j=k-32;
+  			partialDisplay(0x0e - h,0x0e - h,k%256,k/256,j%256,j/256);	// set ram
+  			writeDispRamMono(8, 32, 0x00);	// white
+  	 		updateDisplayPartial();
+  			delay(speed);
+  			partialDisplay(0x0e - h,0x0e - h,k%256,k/256,j%256,j/256);	// set ram
+  			writeDispRamMono(8, 32, 0x00);	// white
+  			delay(speed);
+    	}
+    }
+  }
 }
 #endif
 
@@ -64,14 +69,14 @@ void loop() {
     displayImage(pictures[picture]);
     delay(faster ? 500 : 2000);
   }
-// #ifdef GDEH029A1_H
-//   parttest();
-// #else
+#ifdef GDE_DRIVER_H
+  parttest();
+#else
   faster = !faster;
   initDisplay(faster); // go faster ;)
   if (!faster) {
-    delay(1800);
+    delay(1500);
   }
-// #endif
+#endif
   displayImage(pictures[0]);
 }
